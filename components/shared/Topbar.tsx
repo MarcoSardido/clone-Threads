9abd4f4df7bcd50305871a42ce3035b031405820
Marco Jorge H. Sardido <marcosardido@entropys-MacBook-Pro.local>
2023-09-26 16:35:40 +0800

    Installed shanUI & ZOD; added ZOD validation

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,4 +1,5 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs"
+import { dark } from '@clerk/themes'
 import Image from "next/image"
 import Link from "next/link"
 
@@ -21,8 +22,9 @@ const Topbar = () => {
           </SignedIn>
         </div>
 
-        <OrganizationSwitcher 
+        <OrganizationSwitcher
           appearance={{
+            baseTheme: dark,
             elements: {
               organizationSwitcherTrigger: 'py-2 px-4'
             }
